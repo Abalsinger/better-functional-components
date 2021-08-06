fb9e93412dbfefe8d982bb9008b769a633005a19
Title_of_the_Page <titleofthepage@gmail.com>
2021-08-06 14:40:35 -0400

    List of speakers in a single component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { data } from "./SpeakerData.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function IndexPage() {
+
+    return (
+        <div className="container speakers-list">
+            <div className="row">
+                {data.map(function (speaker) {
+                    const { id, bio, first, last, favorite, twitterHandle, company, sessions } = speaker;
+
+                    return (
+                        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12">
+                            <div className="card card-height p-4 mt-4">
+                                <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
+                                    <img
+                                        className="contain-fit"
+                                        src={'/images/speaker-10801.jpg'}
+                                        width="330"
+                                        alt="Alt" />
+                                </div>
+                                <div className="speaker-info">
+                                    <div className="d-flex justify-content-between mb-3">
+                                        <h3 className="text-truncate w-200">
+                                            {first} {last}
+                                        </h3>
+                                    </div>
+                                    <div>
+                                        <p> {bio} {company} {twitterHandle} {favorite}</p>
+                                    </div>
+                                </div>
+                                <div className="sessionBox card h-250">
+                                    <span className="session w-100">
+                                        {sessions[0].title} <strong>Room: {sessions[0].room.name}</strong>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        )
+                })}
+            </div>
+        </div>
   );
 }
 
-export default App;
+export default IndexPage;
